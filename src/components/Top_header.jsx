d1c6dd1bf8674d8f1b3e87d2a97cf0b9ba50536e
Rituraj Dey <riturajdey01@gmail.com>
2024-03-25 11:56:39 +0530

    Added styles to the Top_header

--- src/components/Top_header.jsx
+++ src/components/Top_header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Airbnb_logo from "../../public/assets/icons/airbnb_main.svg";
-import earth_logo from "../../public/assets/icons/earth.svg";
-import user_logo from "../../public/assets/icons/user.svg";
-import menu_logo from "../../public/assets/icons/menu.svg";
+import Airbnb_logo from "/assets/icons/airbnb_main.svg";
+import earth_logo from "/assets/icons/earth.svg";
+import user_logo from "/assets/icons/user.svg";
+import menu_logo from "/assets/icons/menu.svg";
 
 import './top_header.css';
 function Top_header() {
@@ -17,17 +17,17 @@ function Top_header() {
                     </div>
 
                     <div className="center_items flex gap-5 ml-40">
-                        <div className="text-base p-2 center_items_stays light_color">Stays</div>
-                        <div className="text-base p-2 font-extralight light_color">Experiences</div>
-                        <div className="text-base p-2 font-extralight light_color">Online Experiences</div>
+                        <div className="text-base p-3 center_items_stays light_color">Stays</div>
+                        <div className="text-base p-3 font-extralight light_color">Experiences</div>
+                        <div className="text-base p-3 font-extralight light_color">Online Experiences</div>
                     </div>
 
-                    <div className="right_elements flex items-center gap-4">
+                    <div className="right_elements flex items-center ">
                         <div className=" text-sm airbnb_your_home">Airbnb your home</div>
                         <div>
-                            <img src={earth_logo} alt="earth_logo" />
+                            <img className="earth_logo" src={earth_logo} alt="earth_logo" />
                         </div>
-                        <div className="flex rounded-full border gap-3 p-2 items-center">
+                        <div className=" user_info flex rounded-full border gap-3 p-2 items-center">
                             <div>
                                 <img className="h-4 ml-[3px]" src={menu_logo} alt="menu_logo" />
                             </div>
