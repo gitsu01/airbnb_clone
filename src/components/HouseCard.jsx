a781c1ca977d0570d39888b5ac75fefdd57c6859
Subir Mondal <sprivacc0@gmail.com>
2024-04-04 01:05:29 +0530

    House Listing Part_1

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import HouseImage from '../../public/assets/images/h3.jpg'
 
-function HouseCard(arr) {
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './HouseCard.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+function HouseCard() {
   return (
-    <div>HouseCard</div>
+    <>
+      <div className="house_card w-full">
+        <Swiper
+          pagination={{
+            dynamicBullets: true,
+          }}
+          style={{
+            '--swiper-pagination-color': '#fff',
+          }}
+          lazy={true}
+          loop={true}
+          modules={[Pagination]}
+          className="mySwiper rounded-2xl"
+        >
+          <SwiperSlide><img src={HouseImage} alt="HouseImage" className='aspect-square object-cover' /></SwiperSlide>
+          <SwiperSlide><img src={HouseImage} alt="HouseImage" className='aspect-square object-cover' /></SwiperSlide>
+          <SwiperSlide><img src={HouseImage} alt="HouseImage" className='aspect-square object-cover' /></SwiperSlide>
+          <SwiperSlide><img src={HouseImage} alt="HouseImage" className='aspect-square object-cover' /></SwiperSlide>
+          <SwiperSlide><img src={HouseImage} alt="HouseImage" className='aspect-square object-cover' /></SwiperSlide>
+          
+        </Swiper>
+        
+        <div className="house_info py-4 text-lg">
+          <div>
+            <p className='font-bold'>Schloss Wimsbach, Austria – South Suite</p>
+            <p className='font-extralight'>Built in 1990</p>
+            <p>4-9 Apr</p>
+          </div>
+          <p className="price"><span className='font-bold'>₹12,645</span> night</p>
+        </div>
+      </div>
+
+    </>
   )
 }
 
--- src/components/MainBox.jsx
+++ src/components/MainBox.jsx
@@ -1,12 +1,19 @@
 import './MainBox.css'
-
 import React from 'react'
+import HouseCard from './HouseCard';
 
 function MainBox() {
   return (
     <>
-        <div className="container mx-auto bg-slate-500">
-            cbidcbci    
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-7 gap-y-8">
+            <HouseCard />
+            <HouseCard />
+            <HouseCard />
+            <HouseCard />
+            <HouseCard />
+            <HouseCard />
+            <HouseCard />
+            <HouseCard />
         </div>
     </>
   )
