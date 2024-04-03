f75a02d45ee33ddf15f15b34fb37aa779ab5da99
Subir Mondal <sprivacc0@gmail.com>
2024-04-04 01:10:15 +0530

    Adding Buttom Margin in Main

--- src/components/MainBox.jsx
+++ src/components/MainBox.jsx
@@ -5,7 +5,7 @@ import HouseCard from './HouseCard';
 function MainBox() {
   return (
     <>
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-7 gap-y-8">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-7 gap-y-8 mb-10">
             <HouseCard />
             <HouseCard />
             <HouseCard />
